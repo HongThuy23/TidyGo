e2600447c68a32ec6e436791ced311d684700b44
Hong.Thuy <thuypd15122003@gmail.com>
2025-05-23 13:36:07 +0700

    init project TidyGo

---
+++ src/test/java/com/tidygo/tidygo/TidygoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidygo.tidygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidygoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
